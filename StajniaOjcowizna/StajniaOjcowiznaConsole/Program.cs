64a2e45b060f26d5e3ff7545cbc7f2adda51ecb6
Łukasz Balcerak <ubalcerak@gmail.com>
2021-09-17 20:08:13 +0200

    WebApi start with the LogIn page
    Add web aplication. Add LogIn page with style. Miss logic

--- StajniaOjcowizna/StajniaOjcowiznaConsole/Program.cs
+++ StajniaOjcowizna/StajniaOjcowiznaConsole/Program.cs
@@ -1,5 +1,7 @@
 ﻿using StajniaOjcowiznaCore;
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using StajniaOjcowiznaCore.Control;
 using StajniaOjcowiznaCore.Models;
 using ConsoleTables;
@@ -12,18 +14,21 @@ namespace StajniaOjcowiznaConsole
         {
             JSON.HorseData = JSON.LoadJson<Horse>("Horses");
             JSON.InstructorData = JSON.LoadJson<Instructor>("Instructors");
-            //JSON.LessonsData = JSON.LoadJson<Lesson>("Lessons");
-            DateTime date = new DateTime(2021, 06, 21, 18, 30, 00);
-            TimeSpan time = new TimeSpan(1, 30, 00);
-            Lesson lesson = new Lesson(date, "Indywidualna", 1, time);
-            JSON.LessonsData.Add(lesson);
-            JSON.SaveJson<Lesson>(JSON.LessonsData,"Lessons");
-    
-            
+            JSON.LessonsData = JSON.LoadJson<Lesson>("Lessons");
+
+            //List<Option> options = new List<Option>
+            //{
+            //    new Option("Zapisz jazde", ConsoleView.AddLesson),
+            //    new Option("Usuń jazde", ConsoleView.RemoveLesson)
+            //};
+
+            //ConsoleView.ConsoleMenu(options);
+            //Console.ReadKey();
             ConsoleTable
-                .From<Lesson>(JSON.LessonsData)
+                .From<Horse>(JSON.HorseData)
                 .Configure(o => o.NumberAlignment = Alignment.Left)
                 .Write(Format.MarkDown);
         }
     }
 }
+
--- StajniaOjcowizna/StajniaOjcowiznaCore/Control/Reservations.cs
+++ StajniaOjcowizna/StajniaOjcowiznaCore/Control/Reservations.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using StajniaOjcowiznaCore.Models;
@@ -9,9 +10,9 @@ namespace StajniaOjcowiznaCore.Control
 {
     public class Reservations
     {
-        public static void AddLesson(DateTime lessonDateTime, string lessonType)
+        public static void addLesson()
         {
-
+            Console.WriteLine("Add Lesson");
         }
     }
 }
